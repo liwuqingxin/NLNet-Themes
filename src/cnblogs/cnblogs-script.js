29332ac29db1fac1bba578c2a5d9393b90cd3b00
Liwuqingxin <yangqi1990917@163.com>
2021-02-05 22:55:24 +0800

    cnblogs删除js中最后一行注释，该注释会意外的显示在博客首页；

--- src/cnblogs/cnblogs-script.js
+++ src/cnblogs/cnblogs-script.js
@@ -4,4 +4,3 @@
 
 </script>
 <script src="https://nlnet-iss.oss-cn-hangzhou.aliyuncs.com/img/silence.min.js"></script>
-// https://cdn.jsdelivr.net/gh/esofar/cnblogs-theme-silence@3.0.0-beta4/dist/silence.min.js
