9480533451208bdef07c7002664c25068facd934
Liwuqingxin <yangqi1990917@163.com>
2021-02-05 12:08:47 +0800

    cnblogs添加自定义的github按钮链接；

---
+++ src/cnblogs/cnblogs-script.js
@@ -0,0 +1,6 @@
+<script>
+
+
+
+</script>
+<script src="https://cdn.jsdelivr.net/gh/esofar/cnblogs-theme-silence@3.0.0-beta4/dist/silence.min.js"></script>
--- src/cnblogs/cnblogs.js
+++ src/cnblogs/cnblogs.js
@@ -1,9 +1,7 @@
-<script>
-
 window.$silence = {
     avatar: 'https://s3.ax1x.com/2020/12/21/rBFtd1.png',
     favicon: 'https://s3.ax1x.com/2020/12/21/rBFtd1.png',
-    github: 'https://github.com/liwuqingxin',
+    // github: 'https://github.com/liwuqingxin',
     defaultMode: 'auto',
     defaultTheme: 'c',
 
@@ -134,16 +132,14 @@ $(document).ready(function () {
     return;
     var mainContent = document.getElementById("mainContent");
     var forFlow = mainContent.children[0];
-        
+
     for (var i = 0; i < forFlow.children.length; i++) {
         var cur = forFlow.children[i];
         if (cur != null && cur.className == "day") {
             console.log("in" + cur.className + i);
-            for (var j = 0; j < cur.children.length; j++)
-            {
+            for (var j = 0; j < cur.children.length; j++) {
                 var abs = cur.children[j];
-                if(abs.className == "postCon")
-                {
+                if (abs.className == "postCon") {
                     // $(abs).attr('hidden', 'hidden');
                     abs.innerHTML = "   ";
                 }
@@ -191,5 +187,17 @@ function __js_debug_msg(msg) {
 // 隐藏摘要结束
 
 
-</script>
-<script src="https://cdn.jsdelivr.net/gh/esofar/cnblogs-theme-silence@3.0.0-beta4/dist/silence.min.js"></script>
+// Github
+$(document).ready(function () {
+    var navList = document.getElementById("navList");
+    var a = document.createElement('a');
+    // var img= document.createElement('img');
+    // $(img).attr('src', '');
+    // a.innerText = "Github";
+    a.innerHTML = '<svg class="octicon octicon-mark-github v-align-middle" height="32" viewBox="0 0 16 16" version="1.1" width="32" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>'
+    $(a).attr('href', 'https://github.com/liwuqingxin');
+    $(a).attr('id', 'navList_github');
+    $(a).attr('class', 'github');
+    $(a).attr('target', '_black');
+    navList.appendChild(a);
+});
